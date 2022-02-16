ae1a68b64dc0abfd9553cb968576769382854088
bdeltoro <bdeltoro@live.com>
2022-02-15 20:05:43 -0500

    problem 2

--- practice-for-week-03-recursion-pt1-practice-main/problems/03-ice-cream-shop.js
+++ practice-for-week-03-recursion-pt1-practice-main/problems/03-ice-cream-shop.js
@@ -14,8 +14,22 @@ iceCreamShop([], 'honey lavender'); // false
 
 
 function iceCreamShop(flavors, favorite) {
-  // Your code here
+  let flavor = flavors.pop();
+  // console.log(flavor);
+
+  if(flavor === favorite) {
+    return true
+  } else if (flavor !== favorite) {
+    return iceCreamShop(flavors)
+  };
+return false
+
 }
+console.log(iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')); // true
+console.log(iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')); // false
+iceCreamShop(['moose tracks'], 'moose tracks'); // true
+iceCreamShop([], 'honey lavender'); // false
+iceCreamShop(['vanilla', 'strawberry'], 'blue moon'); // false
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
